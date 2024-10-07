3308fba8ba48789cfaa256ae22fcd3828e8d5438
MitchCas <155246406+Mitchellcass@users.noreply.github.com>
2024-10-06 23:17:22 -0400

    Initialize company structure with departments and employees

---
+++ CC_7.js
@@ -0,0 +1,31 @@
+// Task 1: Create a department structure
+const company = {
+    departments: [
+        {
+            departmentName: 'Engineering',
+            employees: [
+                {
+                    name: 'John',
+                    salary: 100000,
+                    subordinates: []
+                },
+                {
+                    name: 'Clyde',
+                    salary: 80000,
+                    subordinates: []
+                }
+            ]
+        },
+        {
+            departmentName: 'Sales',
+            employees: [
+                {
+                    name: 'Bob',
+                    salary: 90000,
+                    subordinates: []
+                }
+            ]
+        }
+    ]
+};
+
