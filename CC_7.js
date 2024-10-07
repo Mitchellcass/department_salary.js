cb9eacc44ed0f4be86743f9404a3756097f8f3cc
MitchCas <155246406+Mitchellcass@users.noreply.github.com>
2024-10-06 23:35:36 -0400

    Calculate total salary for all departments in the company

--- CC_7.js
+++ CC_7.js
@@ -46,3 +46,18 @@ function calculateDepartmentSalary(department) {
     return totalSalary;
 }
 
+// Task 3: Create a function to calculate the total salary for all departments
+function calculateCompanySalary(company) {
+    let totalCompanySalary = 0;
+
+    company.departments.forEach(department => {
+        // Adding together all departments to total company salary
+        totalCompanySalary += calculateDepartmentSalary(department);
+    });
+
+    return totalCompanySalary; //return the company salary after adding in department salaries
+}
+
+// Test code 
+const totalSalary = calculateCompanySalary(company);
+console.log(`Total company salary is: $${totalSalary}`); // Lets user know the total salary company including departments
